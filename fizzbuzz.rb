80695d7811c2a76824e3b1b0e026f27fc32ca751
mollycann <mollycann4567@gmail.com>
2018-08-07 19:53:54 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++
@@ -1,8 +0,0 @@
-describe "fizzbuzz" do it 'returns "fizz" when the number is divisible by 3' do fizz_3= fizzbuzz(3)
-expect (fizz_3).to eq ("Fizz")
-end 
-end 
-it 'returns' "Fizz" when the number is divisible by 3' do
-end 
-fizzbuzz
-returns "Fizz" when the number is divisible by 3 
