40611719dbf396ac17ee611f52bc95afcdd6d531
mollycann <mollycann4567@gmail.com>
2018-08-07 20:13:30 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,16 @@
+fizzbuzz(3)== "Fizz"
+fizzbuzz(5)== "Buzz"
+fizzbuzz(15)== "FizzBuzz"
+if fizzbuzz=3 return "Fizz"
+  elsif fizzbuzz(5)=5 return "Buzz"
+else fizzbuzz(15)= return "FizzBuzz"
+
+
+
+
+
+
+#we expect fizzbuzz(3) to return "Fizz"
+#we expect fizzbuzz(5) to return"Buzz"
+#we expect fizzbuzz(15) to return "FizzBuzz"
+#we expect fizzbuzz(4) to return nil
